905093062fac961baef6dbb28a77b176e2df1108
Porkalai Selvi.S <58141144+porkalai35@users.noreply.github.com>
2021-07-12 18:12:59 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class Main {
+  public static void main(String[] args) {
+    JFrame frame=new JFrame();
+    Gameplay gamePlay = new Gameplay();
+    
+    frame.setBounds(10, 10, 700, 600);
+    frame.setTitle("Bouncing  Ball");    
+    frame.setResizable(false);
+    frame.setVisible(true);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.add(gamePlay);
+    frame.setVisible(true);
+    
+  }
+
+}
